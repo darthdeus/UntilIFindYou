2286548a99e442372d240cca9cbbb671864ac0ea
Michal Fibich <Michal Fibich>
2016-11-08 21:10:19 +0100

    Sprite Changing Script has been added to match the orientation of the movement (universal script).

---
+++ Assets/Scripts/ModelDirectionChange.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class ModelDirectionChange : MonoBehaviour
+{
+    private Vector2 _current;
+    private SpriteRenderer _default;
+
+    private Sprite _idle;
+    private Sprite _faceLeft;
+    private Sprite _faceUpLeft;
+    private Sprite _faceUp;
+    private Sprite _faceUpRight;
+    private Sprite _faceRight;
+    private Sprite _faceDownRight;
+    private Sprite _faceDown;
+    private Sprite _faceDownLeft;
+    // Use this for initialization
+    void Start()
+    {
+        _current = new Vector2();
+        _default = GetComponent<SpriteRenderer>();
+
+        _idle = Resources.Load<Sprite>(_default.sprite.name + "/Idle");
+        _faceLeft = Resources.Load<Sprite>(_default.sprite.name + "/Left");
+        _faceUpLeft = Resources.Load<Sprite>(_default.sprite.name + "/UpLeft");
+        _faceUp = Resources.Load<Sprite>(_default.sprite.name + "/Up");
+        _faceUpRight = Resources.Load<Sprite>(_default.sprite.name + "/UpRight");
+        _faceRight = Resources.Load<Sprite>(_default.sprite.name + "/Right");
+        _faceDownRight = Resources.Load<Sprite>(_default.sprite.name + "/DownRight");
+        _faceDown = Resources.Load<Sprite>(_default.sprite.name + "/Down");
+        _faceDownLeft = Resources.Load<Sprite>(_default.sprite.name + "/DownLeft");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y;
+        x = _current.x; y = _current.y;
+        _current.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _current.Normalize();
+
+        if (x != _current.x || y != _current.y)
+            SetSprite();
+    }
+
+    /// <summary>
+    /// Checks for current vector and sets the sprite to be matching the orientation.
+    /// </summary>
+    void SetSprite()
+    {
+        if (_current.x > 0)
+        {
+            if (_current.y > 0 && _faceUpRight != null)
+                _default.sprite = _faceUpRight;
+            else if (_current.y == 0 && _faceRight != null)
+                _default.sprite = _faceRight;
+            else if (_current.y < 0 && _faceDownRight != null)
+                _default.sprite = _faceDownRight;
+        }
+        else if (_current.x == 0)
+        {
+            if (_current.y == 0 && _idle != null)
+                _default.sprite = _idle;
+            else if (_current.y > 0 && _faceUp != null)
+                _default.sprite = _faceUp;
+            else if (_current.y < 0 && _faceDown != null)
+                _default.sprite = _faceDown;
+        }
+        else
+        {
+            if (_current.y > 0 && _faceUpLeft != null)
+                _default.sprite = _faceUpLeft;
+            else if (_current.y == 0 && _faceLeft != null)
+                _default.sprite = _faceLeft;
+            else if (_current.y < 0 && _faceDownLeft != null)
+                _default.sprite = _faceDownLeft;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,28 +2,33 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovementController : MonoBehaviour {
+public class PlayerMovementController : MonoBehaviour
+{
 
     public float movementSpeed = 2f;
 
     private Rigidbody2D _rigidbody2D;
 
-    void Start () {
+    void Start()
+    {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update () {
-	    float x = Input.GetAxisRaw("Horizontal");
-	    float y = Input.GetAxisRaw("Vertical");
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
         _rigidbody2D.velocity = new Vector2(x, y).normalized * movementSpeed;
-	}
+    }
 
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D other)
+    {
         var gate = other.collider.GetComponent<GateController>();
 
         // We've collided with a gate
-        if (gate != null) {
+        if (gate != null)
+        {
             Debug.Assert(gate.connectedGate != null);
 
             // TODO - replace this with a scene-specified position
