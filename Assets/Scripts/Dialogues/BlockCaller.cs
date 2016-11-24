343c0e6f0ae7ed3b811e7b366b4bf8fb3cb51890
Michal Fibich <Michal.Fibichidis@gmail.com>
2016-11-24 16:05:07 +0100

    Dialogs Added
    - left-clicking NPC alows talking to it
    - Introduction dialog now gives options
    - Quests are not yet implemented
    - Clicking NPC more than once invokes more than 1 flowchart at once
    (needs fixing)

---
+++ Assets/Scripts/Dialogues/BlockCaller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class BlockCaller : MonoBehaviour
+{
+
+    public string sendingMessage;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        Flowchart.BroadcastFungusMessage(sendingMessage);
+    }
+}
