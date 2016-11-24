c7ee5e524c28417cd72f4ecc108b98282a26a581
Michal Fibich <Michal.Fibichidis@gmail.com>
2016-11-24 21:16:31 +0100

    Fixes
    - Added Reset() method for Rune class
    - Fixed a bug where runes in dialing book remained activated (visually
    they were deactivated, but clicking them did nothing until 2nd click)
    - Fixed a bug where runes could be clicked while dialing book was
    invisible

--- Assets/Scripts/GateTravelling/Rune.cs
+++ Assets/Scripts/GateTravelling/Rune.cs
@@ -3,19 +3,20 @@ using System.Collections;
 using System.Linq;
 using Assets;
 
-public class Rune : MonoBehaviour {
+public class Rune : MonoBehaviour
+{
     private DialingBook _book;
     public string RuneName;
     private AudioSource _sound;
     private bool _alreadyClicked = false;
 
-	void Start () {
-	    _book = DialingBook.Find();
+    void Start()
+    {
+        _book = DialingBook.Find();
         var sounds = GetComponentsInParent<AudioSource>();
 
         foreach (var sound in sounds)
         {
-            
             if (sound.clip.name == "GotItem")
             {
                 _sound = sound;
@@ -24,23 +25,33 @@ public class Rune : MonoBehaviour {
 
     }
 
-    void OnMouseUp() {
+    void OnMouseUp()
+    {
+        if (_book.GetComponent<Renderer>().isVisible)
+        {
+            if (_book.CurrentAddress.Length < 4)
+            {
+                _sound.Play();
+            }
 
-        if (_book.CurrentAddress.Length < 4) {
-            _sound.Play();
+            if (_alreadyClicked)
+            {
+                _book.UnsetSlot();
+                _alreadyClicked = false;
+
+                GetComponent<SpriteRenderer>().material.color = Color.white;
+            }
+            else
+            {
+                _book.SetSlot(RuneName);
+                _alreadyClicked = true;
+
+                GetComponent<SpriteRenderer>().material.color = Color.red;
+            }
         }
-
-        if (_alreadyClicked) {
-            _book.UnsetSlot();
-            _alreadyClicked = false;
-
-            GetComponent<SpriteRenderer>().material.color = Color.white;
-        } else {
-            _book.SetSlot(RuneName);
-            _alreadyClicked = true;
-
-            GetComponent<SpriteRenderer>().material.color = Color.red;
-        }
-        
+    }
+    public void Reset()
+    {
+        _alreadyClicked = false;
     }
 }
