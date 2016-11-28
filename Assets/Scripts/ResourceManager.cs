296a5a42b5f4e11a9cb1c7845a82d8a91a52ea06
Jakub Arnold <darthdeus@gmail.com>
2016-11-28 23:53:47 +0100

    Add resource manager skeleton

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class ResourceManager : MonoBehaviour {
+    public enum ResourceType {
+        Wood,
+        Stone
+    }
+
+    public enum ToolType {
+        Axe,
+        Pickaxe
+    }
+
+    public int ResourceCount(ResourceType type) {
+        switch (type) {
+            case ResourceType.Wood:
+                return 3;
+            case ResourceType.Stone:
+                return 0;
+            default:
+                throw new InvalidOperationException("Invalid resource type");
+
+        }
+    }
+
+    public bool HasTool(ToolType type) {
+        switch (type) {
+            case ToolType.Axe:
+                return true;
+            case ToolType.Pickaxe:
+                return false;
+            default:
+                throw new InvalidOperationException("Invalid tool type");
+        }
+    }
+}
