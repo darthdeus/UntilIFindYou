fcae2dd7f49349cf9688d2b3dd3d8d681349b2b2
Jakub Arnold <darthdeus@gmail.com>
2016-12-02 12:02:46 +0100

    Fix resource sorting layer + book raycast

--- Assets/Scripts/GateTravelling/DialingBook.cs
+++ Assets/Scripts/GateTravelling/DialingBook.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using UnityEngine;
 
-namespace Assets
-{
-    public class DialingBook : MonoBehaviour
-    {
+namespace Assets {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class DialingBook : MonoBehaviour {
         public string CurrentAddress;
         private GateSystem _gateSystem;
         private GameObject _player;
         private AudioSource _success;
         private AudioSource _failure;
         private bool _readyToGo = false;
+        private SpriteRenderer _spriteRenderer;
 
-        void Start()
-        {
+        private bool _isVisible {
+            get { return _spriteRenderer.isVisible; }
+        }
+
+        void Start() {
             _gateSystem = GateSystem.Find();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
             var sounds = GetComponents<AudioSource>();
 
-            foreach (var sound in sounds)
-            {
-                if (sound.clip.name == "Success")
-                {
+            foreach (var sound in sounds) {
+                if (sound.clip.name == "Success") {
                     _success = sound;
-                }
-                else if (sound.clip.name == "Error")
-                {
+                } else if (sound.clip.name == "Error") {
                     _failure = sound;
                 }
             }
@@ -32,22 +33,40 @@ namespace Assets
             SetVisible(false);
         }
 
-        void Update()
-        {
+        void Update() {
             // GATE TRAVELING
-            if (!_success.isPlaying && _readyToGo && _player != null)
-            {
+            if (!_success.isPlaying && _readyToGo && _player != null) {
                 _gateSystem.DialAndMoveAddress(_player, CurrentAddress);
                 _readyToGo = false;
 
                 SetVisible(false);
             }
+
+            // Mouse click raycast to close
+            if (Input.GetMouseButtonUp(0) && _isVisible) {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                var hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+                if (hit) {
+                    var go = hit.collider.gameObject;
+                    var book = go.GetComponent<DialingBook>();
+                    var rune = go.GetComponent<Rune>();
+
+                    if (book != null || rune != null) {
+                        Debug.Log("Book clicked");
+                    } else {
+                        SetVisible(false);
+                        Debug.Log("Hiding book");
+                    }
+                } else if (_isVisible) {
+                    SetVisible(false);
+                    Debug.Log("Clicked outside the book, hiding");
+                }
+            }
         }
 
-        public void Activate(GameObject player)
-        {
-            if (!_readyToGo)
-            {
+        public void Activate(GameObject player) {
+            if (!_readyToGo) {
                 _player = player;
                 ResetBook();
                 ResetRunes();
@@ -56,24 +75,18 @@ namespace Assets
             }
         }
 
-        public void SetSlot(string slot)
-        {
-            if (CurrentAddress.Length < 5)
-            {
+        public void SetSlot(string slot) {
+            if (CurrentAddress.Length < 5) {
                 CurrentAddress += slot;
                 Debug.Log(String.Format("SETTING: {0}", CurrentAddress));
 
-                if (CurrentAddress.Length == 5)
-                {
+                if (CurrentAddress.Length == 5) {
                     Debug.Assert(_player != null, "Dialing wasn't activated, player is missing");
 
-                    if (_gateSystem.CheckCorrectAddress(CurrentAddress))
-                    {
+                    if (_gateSystem.CheckCorrectAddress(CurrentAddress)) {
                         _success.Play();
                         _readyToGo = true;
-                    }
-                    else
-                    {
+                    } else {
                         _failure.Play();
                         SetVisible(false);
                     }
@@ -81,18 +94,15 @@ namespace Assets
             }
         }
 
-        public void UnsetSlot()
-        {
-            if (CurrentAddress.Length > 0 && CurrentAddress.Length < 5)
-            {
+        public void UnsetSlot() {
+            if (CurrentAddress.Length > 0 && CurrentAddress.Length < 5) {
                 CurrentAddress = CurrentAddress.Substring(0, CurrentAddress.Length - 1);
             }
 
             Debug.Log(String.Format("UNSETTING: {0}", CurrentAddress));
         }
 
-        public static DialingBook Find()
-        {
+        public static DialingBook Find() {
             var objects = GameObject.FindGameObjectsWithTag("DialingBook");
             Debug.Assert(objects.Length == 1, "There should be only one DialingBook");
 
@@ -102,36 +112,29 @@ namespace Assets
         }
 
         // PRIVATE
-        private void SetVisible(bool visible)
-        {
+        private void SetVisible(bool visible) {
             GetComponent<SpriteRenderer>().enabled = visible;
-            foreach (var r in GetComponentsInChildren<SpriteRenderer>())
-            {
+            foreach (var r in GetComponentsInChildren<SpriteRenderer>()) {
                 r.enabled = visible;
             }
         }
 
-        private void ResetRunes()
-        {
-            foreach (var r in GetComponentsInChildren<SpriteRenderer>())
-            {
+        private void ResetRunes() {
+            foreach (var r in GetComponentsInChildren<SpriteRenderer>()) {
                 r.material.color = Color.white;
             }
 
-            foreach (var rune in GetComponentsInChildren<Rune>())
-            {
+            foreach (var rune in GetComponentsInChildren<Rune>()) {
                 rune.Reset();
             }
         }
 
-        private void ResetBook()
-        {
+        private void ResetBook() {
             CurrentAddress = "";
             SetVisible(false);
         }
 
-        private void MoveBook()
-        {
+        private void MoveBook() {
             var x = _player.transform.position.x;
             var y = _player.transform.position.y;
             gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
