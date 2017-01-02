85cf2e1c34b5c00036bfddb3e1247dc767fd9cdb
Zuzana Dostalova <dostalovaz@gmail.com>
2017-01-02 22:50:08 +0100

    Intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject Image1;
+    public GameObject Image2;
+    public GameObject Image3;
+    public GameObject Image4;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
