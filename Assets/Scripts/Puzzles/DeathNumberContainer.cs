0361edbde973e766d81b2e7fc23b4b6e5b27a816
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-01-06 00:02:54 +0100

    Puzzle Progress
    - Graphics added
    - Basic Functionalites added
    - Solution Check missing
    - Puzzle hiding and showing missing

---
+++ Assets/Scripts/Puzzles/DeathNumberContainer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathNumberContainer : MonoBehaviour {
+
+	public Sprite Number0;
+	public Sprite Number1;
+	public Sprite Number2;
+	public Sprite Number3;
+	public Sprite Number4;
+	public Sprite Number5;
+	public Sprite Number6;
+	public Sprite Number7;
+	public Sprite Number8;
+	public Sprite Number9;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
