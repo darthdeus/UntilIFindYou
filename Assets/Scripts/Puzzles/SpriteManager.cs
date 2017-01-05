f4767148bec72110cbf7819f34e50dbea203af0a
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-01-05 16:31:27 +0100

    Added a puzzle to the IcyPlanet
    Most important changes:
    - Added a flowchart IcyPlanetPuzzle
    - Modified Intro flowchart to hide the Frost rune
    - Added 3 new scripts for the puzzle
    - Added 4 tables to the IcyPlanet
    - Added scripts to IcyPlanet gameobjects (mouse scripts and puzzle
    scripts)
    - Changed layer order for IcyPlanet objects (they were not visible for
    me)
    Bugs & Issues:
    - Flowchart does not show background for Menu options on IcyPlanet
    - IcyPlanet does not change its appearance after the puzzle is fully
    finished
    - Frost rune is not really a Frost rune, but some other rune from old
    runes

---
+++ Assets/Scripts/Puzzles/SpriteManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Carrot()
+    {
+        GetComponent<SpriteRenderer>().sprite = gameObject.transform.parent.GetComponent<TableSpriteHolder>().CarrotTable;
+    }
+
+    public void Scarf()
+    {
+        GetComponent<SpriteRenderer>().sprite = gameObject.transform.parent.GetComponent<TableSpriteHolder>().ScarfTable;
+    }
+
+    public void Snowflakes()
+    {
+        GetComponent<SpriteRenderer>().sprite = gameObject.transform.parent.GetComponent<TableSpriteHolder>().SnowflakesTable;
+    }
+
+    public void Icicle()
+    {
+        GetComponent<SpriteRenderer>().sprite = gameObject.transform.parent.GetComponent<TableSpriteHolder>().IcicleTable;
+    }
+    public void Default()
+    {
+        GetComponent<SpriteRenderer>().sprite = gameObject.transform.parent.GetComponent<TableSpriteHolder>().DefaultTable;
+    }
+}
---
+++ Assets/Scripts/Puzzles/TableSpriteHolder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TableSpriteHolder : MonoBehaviour
+{
+
+    public Sprite DefaultTable;
+    public Sprite CarrotTable;
+    public Sprite IcicleTable;
+    public Sprite ScarfTable;
+    public Sprite SnowflakesTable;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
