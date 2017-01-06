a9d7a623b3c861be4d0b4e22f26d4f214eb34533
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-01-06 01:11:21 +0100

    Death Planet Puzzle Finished

---
+++ Assets/Scripts/Puzzles/DeathCollectPaper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathCollectPaper : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("other.gameObject.tag" + other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("PaperFound");
+            Destroy(gameObject);
+        }
+    }
+}
