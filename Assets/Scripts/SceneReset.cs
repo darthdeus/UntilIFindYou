9e5a008b79fa61bde73e2de46d4b868fa5a0cd3f
Michal Fibich <Michal.Fibichidis@gmail.com>
2017-01-06 21:11:27 +0100

    The game now closes after losing the game

--- Assets/Scripts/SceneReset.cs
+++ Assets/Scripts/SceneReset.cs
@@ -2,23 +2,30 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class SceneReset : MonoBehaviour {
+public class SceneReset : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void ResetScene()
     {
         Fungus.Flowchart.BroadcastFungusMessage("EarthQuakeFade");
-        // TODO HOW TO RESET SCENE? OR ANY OTHER GAME ENDING SHOULD GO HERE
-        //SceneManager.LoadScene("Main");
+        StartCoroutine(WaitFor(3.6f));
+    }
+    IEnumerator WaitFor(float Seconds)
+    {
+        yield return new WaitForSeconds(Seconds);
+        Application.Quit();         // Won't Work in Unity debug mode - works in build //
         Debug.Log("Game ended");
     }
 }
